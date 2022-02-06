def330a93f25c314d58fe3ac71e11c31fee952e8
cureforoptimism <cure_for_optimism@protonmail.com>
2022-02-06 15:49:57 -0700

    Fix emoji

--- src/main/java/com/cureforoptimism/discordbase/Constants.java
+++ src/main/java/com/cureforoptimism/discordbase/Constants.java
@@ -1,5 +1,5 @@
 package com.cureforoptimism.discordbase;
 
 public class Constants {
-  public static final String EMOJI_PIXILFRAME_039 = "<a:pixilframe039:940007922145050644>";
+  public static final String EMOJI_PIXILFRAME_039 = "<:pixilframe039:939151147657220096>";
 }
