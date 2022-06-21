118712e09cde1067bc40c00add370d86b527b301
Cure For Optimism <cure_for_optimism@protonmail.com>
2022-06-21 00:07:16 -0600

    Update to actual sales channel

--- src/main/java/com/cureforoptimism/discordbase/Constants.java
+++ src/main/java/com/cureforoptimism/discordbase/Constants.java
@@ -4,6 +4,5 @@ public class Constants {
   public static final String BIRDS_CONTRACT_ID = "0x4C1054dFF878614a30164222625b1D1BfD819873";
   public static final long CHANNEL_TEST_GENERAL = 926303694801223751L;
 
-  // TODO: Update with live sales channel
-  public static final long CHANNEL_SALES_BOT = 937917497112424500L;
+  public static final long CHANNEL_SALES_BOT = 907881425569734686L;
 }
